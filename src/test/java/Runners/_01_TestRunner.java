64a089392570e2d4488d97c37eb39d84f3ade523
EsraBaylar <esrabaylar.29@gmail.com>
2021-09-16 22:08:41 +0300

    jenkins icin poma built eklendi.Pom.xml e plugin eklendi.

--- src/test/java/Runners/_01_TestRunner.java
+++ src/test/java/Runners/_01_TestRunner.java
@@ -19,8 +19,14 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles"},  // buraya feature files ın path i verilecek.
         glue = {"StepDefinitions"},     // Step definitions ın klasörü yazılıyor.
-        dryRun = false    // true olduğunda testi çalıştırma sadece feature lara ait steplerin varlığını kontrol eder.
+        dryRun = false ,   // true olduğunda testi çalıştırma sadece feature lara ait steplerin varlığını kontrol eder.
                           // false olduğunda ise testi çalıştırır.
+
+        plugin = { //basit rapor oluşturan plugin
+        "html:target/cucumber-report",
+        "json:target/cucumber.json",
+}
+
 )
 public class _01_TestRunner extends AbstractTestNGCucumberTests {
 
